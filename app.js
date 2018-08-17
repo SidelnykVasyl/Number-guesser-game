6e39c1c45a45baf9f2863d7e6b04ab4b70be85f1
User <Sidelnykv@gmail.com>
2018-08-17 18:28:06 +0300

    Commit

---
+++ app.js
@@ -0,0 +1,111 @@
+/* 
+ Умови гри!
+ - Гравці мають вгадати число між мінімальним і максимальним!
+ - Гравці мають певну кількість припущень (спроб) !
+ - Інформувати гравця про кількість спроб, які залишились!
+ - Інформувати гравця про правильну або неправильну відповіді!
+ - Надавати можливість спробувати ще раз!
+*/
+
+// Game Values 
+
+let min = 1,
+    max = 10,
+    winningNum = getRandomNum(min, max),
+    guessesLeft = 3;
+
+// UI Elements
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if (e.target.className === 'play-again'){
+    window.location.reload();
+  }
+});
+
+// Listen for guess
+
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+
+  // Validate 
+  if(isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+  }
+
+  // Check if won
+  if(guess === winningNum){
+    // Game over - won
+    gameOver(true, `${winningNum} is correct, YOU WON!`);
+
+  }else{
+    // Wrong number 
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
+      //Game over - lost
+
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+
+    }else{
+      // Game continues - answer wrong
+
+      // Change border color
+
+      guessInput.style.borderColor = 'red';
+
+      // Clear Input
+      guessInput.value = '';
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+      
+    }
+  }
+
+});
+
+// Game Over
+function gameOver(won, msg){
+   let color;
+   won === true ? color = 'green' : color = 'red';
+
+// Disable input
+guessInput.disabled = true;
+// Change border color
+guessInput.style.borderColor = color;
+// Set text color
+message.style.color = color;
+// Set message 
+setMessage(msg);
+
+// Play again
+guessBtn.value = 'Play Again';
+guessBtn.className += 'play-again'
+}
+
+// Get winning number 
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+// Set message 
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
+
+      
